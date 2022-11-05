fea1d86c265235f9059d0faa4ea9f8e434fd8381
rabeeh-ta <rabeehchemnad@gmail.com>
2022-11-05 23:47:57 +0530

    ADDED: about me section

---
+++ src/Views/AboutMe.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function AboutMe() {
+  return (
+    <div className="container-fluid tickets" id="tickets">
+      <div className="row me-row content-ct">
+        <h2 className="row-title">About Me</h2>
+        <div className="col-md-4 col-sm-6 col-md-offset-2">
+          <p>
+            I am a self taught Web Developer / Programmer for the past 3 years
+            and is still learning for the long run. I have my expertise in HTML,
+            CSS, JS and some other frameworks like Vue.js and Flutter, also do
+            code C++ for my DIY projects.
+          </p>
+        </div>
+        <div className="col-md-4 col-sm-6">
+          <p>
+            I love building this over buying so i do build a lot of things. I
+            also write programmes for Arduinos and other IOT boards which is my
+            main hobby.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Landing from './Views/Landing';
-import FeaturedProjects from './Views/FeaturedProjects';
+import Landing from './views/Landing';
+import FeaturedProjects from './views/FeaturedProjects';
+import AboutMe from './views/AboutMe';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Landing />
     <FeaturedProjects />
+    <AboutMe />
   </React.StrictMode>
 );
